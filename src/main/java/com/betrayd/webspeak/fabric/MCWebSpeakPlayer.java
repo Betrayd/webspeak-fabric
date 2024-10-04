99a569c996626b867585af2c2bd7067e792d4b26
Sam54123 <thesam54123@gmail.com>
2024-10-03 22:45:17 -0700

    implemented audio spatialization

--- src/main/java/com/betrayd/webspeak/fabric/MCWebSpeakPlayer.java
+++ src/main/java/com/betrayd/webspeak/fabric/MCWebSpeakPlayer.java
@@ -1,14 +1,23 @@
 package com.betrayd.webspeak.fabric;
 
+import org.joml.Vector3d;
+import org.joml.Vector3f;
+import org.slf4j.LoggerFactory;
+
+import com.betrayd.webspeak.fabric.util.WebSpeakUtils;
+
 import net.betrayd.webspeak.WebSpeakPlayer;
 import net.betrayd.webspeak.WebSpeakServer;
 import net.betrayd.webspeak.util.WebSpeakVector;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 
 public class MCWebSpeakPlayer extends WebSpeakPlayer {
 
     private ServerPlayerEntity mcPlayer;
+
+    private Vector3f forwardVector = new Vector3f();
 
     public MCWebSpeakPlayer(WebSpeakServer server, ServerPlayerEntity player, String sessionId) {
         super(server, player.getUuidAsString(), sessionId);
@@ -35,7 +44,18 @@ public class MCWebSpeakPlayer extends WebSpeakPlayer {
     public WebSpeakVector getLocation() {
         // Use eye position because that's where player is speaking & listening from
         Vec3d eyePos = mcPlayer.getEyePos();
-        return new WebSpeakVector(-eyePos.getX(), eyePos.getZ(), eyePos.getY());
+        return new WebSpeakVector(eyePos.getX(), eyePos.getY(), eyePos.getZ());
+    }
+
+    @Override
+    public WebSpeakVector getForward() {
+        var vec = Vec3d.fromPolar(mcPlayer.getPitch(), mcPlayer.getYaw());
+        return new WebSpeakVector(vec.x, vec.y, vec.z);
+        // forwardVector.set(0, 1, 0);
+        // forwardVector.rotateX(mcPlayer.getPitch() * MathHelper.RADIANS_PER_DEGREE);
+        // forwardVector.rotateZ(mcPlayer.getYaw() * MathHelper.RADIANS_PER_DEGREE);
+        // // LoggerFactory.getLogger(getClass()).info("Vec: {}", forwardVector);
+        // return new WebSpeakVector(forwardVector.x, forwardVector.y, forwardVector.z);
     }
 
     @Override
